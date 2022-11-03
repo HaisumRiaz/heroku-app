77a8f0ab1a81d5b15c59f07fc8f633e862173ea7
haisumriaz <haisumriaz@gmail.com>
2022-11-03 16:24:13 +0500

    hapi not happy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Chalo G
+          Hapi not Happy
         </p>
         <a
           className="App-link"
