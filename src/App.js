968321f1f5dcfc5995c9016084b180b25f531558
haisumriaz <haisumriaz@gmail.com>
2022-11-15 18:51:12 +0500

    app2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          App1
+          App2
         </p>
         <a
           className="App-link"
